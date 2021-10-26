041901a59eba94bdb82a031c4171b132bf5e8cc1
Kit <kit@Kits-M1-Air.local>
2021-10-26 11:19:41 -0500

    add install-plugins.sh

---
+++ install-plugins.sh
@@ -0,0 +1,7 @@
+# install zsh-syntaz-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
