daeabee1121318ecda9a2f950a1eff34000c9c35
Kit <kit@Kits-M1-Air.local>
2022-07-02 21:55:39 -0500

    create vim directories in script

--- scripts/install-rc-files.sh
+++ scripts/install-rc-files.sh
@@ -1,6 +1,11 @@
 # install to .zshrc
 echo "[[ -f ~/dotfiles/zshrc ]] && source ~/dotfiles/zshrc" >> ~/.zshrc
 
+# create vim directories
+mkdir -p ~/.vim/.undo
+mkdir -p ~/.vim/.backup
+mkdir -p ~/.vim/.swp
+
 # install to .vimrc
 echo 'if filereadable($HOME . "/dotfiles/vimrc")
   source ~/dotfiles/vimrc
