81915ec9e1cdffc6f9a497c094e511ff495e9af9
Kit <kit@Kits-M1-Air.local>
2023-01-17 21:45:10 -0600

    install a newer version of neovim with apt

--- scripts/install-neovim.sh
+++ scripts/install-neovim.sh
@@ -2,6 +2,8 @@
 if [ -x "$(command -v brew)" ]; then
   brew install neovim ripgrep
 else
+  sudo add-apt-repository ppa:neovim-ppa/stable
+  sudo apt update
   sudo apt install -y neovim ripgrep
 fi
 
