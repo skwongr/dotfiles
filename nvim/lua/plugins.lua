bcb9d6ab433c8ca5298d79459a26b80527711f41
Kit <kit@Kits-M1-Air.local>
2023-01-19 14:05:18 -0600

    add buffer to auto-complete

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -40,18 +40,30 @@ end)
 -- Setup nvim-cmp
 local cmp = require'cmp'
 
+-- limit buffer size to index for auto-complete
+local cmp_get_bufnrs = function()
+  local buf = vim.api.nvim_get_current_buf()
+  local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+  if byte_size > 3 * 1024 * 1024 then -- 3 Megabyte max
+    return {}
+  end
+  return { buf }
+end
+
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-n'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  })
-})
--- filetype specific
-cmp.setup.filetype('ruby', {
+  }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' }
   }, {
-    { name = 'buffer' }
+    {
+      name = 'buffer',
+      option = {
+        get_bufnrs = cmp_get_bufnrs
+      }
+    }
   })
 })
 
