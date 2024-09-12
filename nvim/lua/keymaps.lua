6290666424bcd92f6179e9e2bd6d9fb281272f67
Kit Wong <siuw@amazon.com>
2024-09-12 12:00:59 -0500

    update keymaps and ts_ls

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,17 +1,11 @@
 local wk = require('which-key')
 local telescope_builtin = require('telescope.builtin')
 
-wk.register({
-  ['<leader>'] = {
-    name = 'Leader',
-    f = {
-      name = 'Find',
-      f = { telescope_builtin.find_files, 'Find files' },
-      g = { telescope_builtin.live_grep, 'Live grep' },
-    },
-    l = {
-      name = 'Language',
-      v = { '<Plug>(toggle-lsp-diag-vtext)', 'Toggle virtual text' },
-    }
-  },
+wk.add({
+  { "<leader>", group = "Leader" },
+  { "<leader>f", group = "Find" },
+  { "<leader>ff", telescope_builtin.find_files, desc = "Find files" },
+  { "<leader>fg", telescope_builtin.live_grep, desc = "Live grep" },
+  { "<leader>l", group = "Language" },
+  { "<leader>lv", "<Plug>(toggle-lsp-diag-vtext)", desc = "Toggle virtual text" },
 })
