15d679aee2d4d1d3753b7ed17020c4c4dcf6236e
Kit Wong <siuw@amazon.com>
2025-04-03 13:23:11 -0500

    add treesitter

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -31,6 +31,14 @@ require('packer').startup(function(use)
   }
 
   use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+        ts_update()
+    end,
+  }
+
+  use {
     "folke/which-key.nvim",
     config = function()
       vim.o.timeout = true
@@ -87,3 +95,25 @@ cmp.setup({
 
 -- Setup telescope
 require('telescope').load_extension('fzf')
+
+-- Setup treesitter
+require('nvim-treesitter.configs').setup({
+  highlight = {
+    enable = true,
+
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+})
